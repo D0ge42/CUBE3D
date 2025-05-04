de4b36b2b9e5f8820a0907e9bd11bc16b089a2c8
Liviana Dei Svaldi <ldei-sva@e3r2p2.42roma.it>
2025-05-04 20:22:47 +0200

    tried to draw walls..

--- srcs/raycasting/draw_map.c
+++ srcs/raycasting/draw_map.c
@@ -33,12 +33,19 @@ void	draw_wall(int x, int y, t_ray *ray, t_data *data)
 {
 	//char	**map;
 	double		distance;
+	int			i;
 
 	(void) ray;
+	i = 0;
 	//map = data->map->map;
 	if (ray->side == 0)
 		distance = (x - data->player->pos_x + (1 - ray->ray_dir_x) / 2) / ray->ray_x;
 	else
 		distance = (y - data->player->pos_y + (1 - ray->ray_dir_y) / 2) / ray->ray_y;
-	printf("%f\n", distance);
+	distance *= 30;
+	while (i < distance && i < HEIGHT)
+	{
+		my_mlx_pixel_put(data, x * 30, (y * 30) + i, 0xFFFFFF);
+		i++;
+	}
 }
