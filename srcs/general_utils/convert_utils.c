b98fac2ee067d64ebf727db5cd551187e563f7a4
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-05-09 11:02:46 +0200

    Added more checks on color

--- srcs/general_utils/convert_utils.c
+++ srcs/general_utils/convert_utils.c
@@ -1,7 +1,9 @@
 #include "cube.h"
+#include "libft.h"
 #include <stdlib.h>
 
 //static int ft_atoi_base(char *color);
+static void check_color_validity(t_data *data, char *str);
 
 void rgb_converter(t_data *data, char x)
 {
@@ -16,14 +18,34 @@ void rgb_converter(t_data *data, char x)
     exit(EXIT_FAILURE);
   }
   unsigned int *rgb = ft_calloc(3,sizeof(unsigned int));
+  if (rgb == NULL)
+  {
+    ft_putstr_fd(ERR_MALLOC_FAIL,2);
+    free_everything(data);
+    exit(EXIT_FAILURE);
+  }
   int i = 0;
   int j = 0;
   while(str[i] && !ft_isdigit(str[i]))
     i++;
   char **tmp_color = ft_split(&str[i],',');
+  if (tmp_color == NULL)
+  {
+    ft_putstr_fd(ERR_MALLOC_FAIL,2);
+    free_everything(data);
+    exit(EXIT_FAILURE);
+  }
   while(tmp_color[j] != NULL)
   {
+    check_color_validity(data,tmp_color[j]);
     rgb[j] = ft_atoi(tmp_color[j]);
+    if (rgb[j] > 255)
+    {
+      free_everything(data);
+      ft_putstr_fd("COLOR VALUE MUST BE IN RANGE [0,255]\n",2);
+      exit(EXIT_FAILURE);
+    }
+
     j++;
   }
   free_strs(tmp_color);
@@ -31,6 +53,26 @@ void rgb_converter(t_data *data, char x)
     data->map->f_rgb = rgb[0] << 16 | rgb[1] << 8 | rgb[2];
   else if (x == 'C')
     data->map->c_rgb = rgb[0] << 16 | rgb[1] << 8 | rgb[2];
+}
+
+static void check_color_validity(t_data *data, char *str)
+{
+  int i = 0;
+  while(str[i])
+  {
+    if (str[i] == '\n')
+    {
+      i++;
+      continue;
+    }
+    if (!ft_isdigit(str[i]))
+    {
+      ft_putstr_fd("Color can only contain digits\n",2);
+      free_everything(data);
+      exit(EXIT_FAILURE);
+    }
+    i++;
+  }
 }
 
 /*int	get_nb(char c, char *base)
