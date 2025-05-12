2394b29fd79d324c35e582f363f19aca1ee8f4b5
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-05-12 13:56:25 +0200

    Updated parsing

--- srcs/general_utils/convert_utils.c
+++ srcs/general_utils/convert_utils.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 //static int ft_atoi_base(char *color);
-static void check_color_validity(t_data *data, char *str);
+static void check_color_validity(t_data *data, char *str, unsigned int *rgb, char **tmp_color);
 static void	check_overflow(t_data *data, char *str);
 
 void rgb_converter(t_data *data, char x)
@@ -22,6 +22,7 @@ void rgb_converter(t_data *data, char x)
   if (rgb == NULL)
   {
     ft_putstr_fd(ERR_MALLOC_FAIL,2);
+    free(rgb);
     free_everything(data);
     exit(EXIT_FAILURE);
   }
@@ -39,12 +40,13 @@ void rgb_converter(t_data *data, char x)
   if (tmp_color == NULL)
   {
     ft_putstr_fd(ERR_MALLOC_FAIL,2);
+    free(rgb);
     free_everything(data);
     exit(EXIT_FAILURE);
   }
   while(tmp_color[j] != NULL)
   {
-    check_color_validity(data,tmp_color[j]);
+    check_color_validity(data,tmp_color[j],rgb,tmp_color);
     check_overflow(data,tmp_color[j]);
     rgb[j] = ft_atoi(tmp_color[j]);
     if (rgb[j] > 255)
@@ -82,7 +84,7 @@ static void	check_overflow(t_data *data, char *str)
 	}
 }
 
-static void check_color_validity(t_data *data, char *str)
+static void check_color_validity(t_data *data, char *str, unsigned int *rgb, char **tmp_color)
 {
   int i = 0;
   while(str[i])
@@ -95,6 +97,8 @@ static void check_color_validity(t_data *data, char *str)
     if (!ft_isdigit(str[i]))
     {
       ft_putstr_fd(ERR_ONLY_DIGITS,2);
+      free_strs(tmp_color);
+      free(rgb);
       free_everything(data);
       exit(EXIT_FAILURE);
     }
