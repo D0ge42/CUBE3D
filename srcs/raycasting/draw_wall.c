bf8b64a4e92a813979a3389eb13c81e5d9a84f9c
Liviana Dei Svaldi <ldei-sva@e3r6p1.42roma.it>
2025-05-14 21:05:10 +0200

    texture fixed for y

--- srcs/raycasting/draw_wall.c
+++ srcs/raycasting/draw_wall.c
@@ -1,27 +1,35 @@
 #include "cube.h"
 
-unsigned int	get_color(t_ray *ray, t_data *data, int x, int y)
+unsigned int	get_color(t_ray *ray, t_data *data, int height, int y)
 {
 	unsigned int	color;
+	t_texture		*texture;
+	double				x;
 
+	printf("%f\n", ray->hitpoint_x);
 	if (ray->identifier == 'P')
-		return (*(data->door[0]->img_ptr + (((x % data->door[0]->width) + ((y % data->door[0]->height) * data->door[0]->width)))));
+		texture = data->door[0];
 	else if (ray->identifier == 'O')
-		return (*(data->door[3]->img_ptr + (((x % data->door[3]->width) + ((y % data->door[3]->height) * data->door[3]->width)))));
-	if (ray->side == 0)
+		texture = data->door[3];
+	else if (ray->side == 0)
 	{
 		if (ray->ray_dir_x < 0)
-			color = *(data->west->img_ptr + (((x % data->west->width) + ((y % data->west->height) * data->west->width))));
+			texture = data->west;
 		else
-			color = *(data->est->img_ptr + ((x % data->est->width) + ((y % data->est->height) * data->est->width)));
+			texture = data->est;
 	}
 	else
 	{
 		if (ray->ray_dir_y > 0)
-			color = *(data->sud->img_ptr + ((x % data->sud->width) + ((y % data->sud->height) * data->sud->width)));
+			texture = data->sud;
 		else
-			color = *(data->nord->img_ptr + (((x % data->nord->width) + ((y % data->nord->height) * data->nord->width))));
+			texture = data->nord;
 	}
+	x = (ray->hitpoint_x - floor(ray->hitpoint_x)) * 10;
+	// printf("%f %f\n", x, ray->hitpoint_x);
+	if (height != 0)
+		y = y * texture->height / height;
+	color = *(texture->img_ptr + ((((int)x % texture->width) + ((y % texture->height) * texture->width))));
 	return (color);
 }
 
@@ -34,26 +42,22 @@ void	draw_wall(int x, int y, t_ray *ray, t_data *data)
 	unsigned int	color;
 
 	i = 0;
-	k = HEIGHT / 2;
+
 	if (ray->side == 0)
 		distance = (x - data->player->pos_x + (1 - ray->ray_dir_x) / 2) / ray->ray_x;
 	else
 		distance = (y - data->player->pos_y + (1 - ray->ray_dir_y) / 2) / ray->ray_y;
 	height = (HEIGHT / distance);
+	k = (HEIGHT / 2) - (height / 2);
 	ray->hitpoint_x = data->player->pos_x + (distance * ray->dist_x);
 	ray->hitpoint_y = data->player->pos_y + (distance * ray->dist_y);
-	while (i < height / 2)
+	//printf("%f\n", ray->hitpoint_x);
+	if (k + i < 0)
+		i = -k;
+	while (i < height && (k + i) < HEIGHT)
 	{
-		if ((k + i) < HEIGHT)
-		{
-			color = get_color(ray, data, (ray->x ), (k + i));
-			my_mlx_pixel_put(data, ray->x, k + i, color);
-		}
-		if ((k - i) > 0)
-		{
-			color = get_color(ray, data, (ray->x), (k - i));
-			my_mlx_pixel_put(data, ray->x, k - i, color);
-		}
+		color = get_color(ray, data, height, i);
+		my_mlx_pixel_put(data, ray->x, k + i, color);
 		i++;
 	}
 }
