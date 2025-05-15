a4d55d2b35ae07654d1b8b41a741195d7fadc272
Liviana <liviana.deisvaldi@gmail.com>
2025-05-15 15:54:02 +0200

    prova 4

--- srcs/raycasting/draw_wall.c
+++ srcs/raycasting/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: ldei-sva <ldei-sva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:41:27 by ldei-sva          #+#    #+#             */
-/*   Updated: 2025/05/15 15:50:53 by ldei-sva         ###   ########.fr       */
+/*   Updated: 2025/05/15 15:52:53 by ldei-sva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	draw_wall(int x, int y, t_ray *ray, t_data *data)
 	int				height;
 	int				i;
 	int				k;
-	double			prova;
+	//double			prova;
 	unsigned int	color;
 
 	i = 0;
@@ -62,13 +62,13 @@ void	draw_wall(int x, int y, t_ray *ray, t_data *data)
 		distance = (y - data->player->pos_y + (1 - ray->ray_dir_y) / 2) / ray->ray_y;
 	if (ray->side == 0)
 	{
-		prova = ray->side_dist_x - ray->dist_x;
-		ray->hitpoint_x = data->player->pos_x + ray->ray_dir_x * prova;
+		//prova = ray->side_dist_x - ray->dist_x;
+		ray->hitpoint_x = data->player->pos_x + ray->ray_dir_x * distance;
 	}
 	else
 	{
-		prova = ray->side_dist_y - ray->dist_y;
-		ray->hitpoint_x = data->player->pos_y + ray->ray_dir_y * prova;
+		//prova = ray->side_dist_y - ray->dist_y;
+		ray->hitpoint_x = data->player->pos_y + ray->ray_dir_y * distance;
 	}
 	height = (HEIGHT / distance);
 	k = (HEIGHT / 2) - (height / 2);
